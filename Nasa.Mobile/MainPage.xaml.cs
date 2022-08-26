30dbff6caafc9cb4658e944fea2682ebc97462fb
David Pine <david.pine.7@gmail.com>
2022-08-26 15:14:06 -0500

    A few more tweaks

--- Nasa.Mobile/MainPage.xaml.cs
+++ Nasa.Mobile/MainPage.xaml.cs
@@ -2,31 +2,27 @@
 
 namespace Nasa.Mobile;
 
-public sealed partial class MainPage : ContentPage, IDisposable
+public sealed partial class MainPage : ContentPage
 {
-    private readonly ApodClient _client = null!;
-
     public readonly DateTime MinDate = new(1995, 6, 20);
     public readonly DateTime MaxDate = DateTime.Today;
 
     public DateTime SelectedDate { get; set; } = DateTime.Today;
 
-    public MainPage()
-    {
-        InitializeComponent();
-        _client = Handler.MauiContext.Services.GetRequiredService<ApodClient>();
-    }
+    public MainPage() => InitializeComponent();
 
-    private async void OnCounterClicked(object sender, EventArgs e)
+    private async void OnCounterClicked(object sender, EventArgs e) =>
+        await LoadRandomImageAsync();
+
+    private async Task LoadRandomImageAsync()
     {
-        var response = await _client.FetchApodAsync(1);
+        var client = Handler.MauiContext.Services.GetRequiredService<ApodClient>();
+        var response = await client.FetchApodAsync(1);
         if (response is { StatusCode: ApodStatusCode.OK })
         {
             _image.Source = response.Content.ContentUrlHD;
             SemanticScreenReader.Announce(response.Content.Explanation);
         }
     }
-
-    void IDisposable.Dispose() => _client?.Dispose();
 }
 
